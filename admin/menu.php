5160257866807cc1fe5868b5750e708e719f318f
rocketpastsix <mjftrask@gmail.com>
2014-10-16 22:34:47 -0400

    need coffee

--- admin/menu.php
+++ admin/menu.php
@@ -9,6 +9,7 @@ if(!$user->is_logged_in()){ header('Location: login.php'); }
 <h1>Blog</h1>
 <ul id='adminmenu'>
 	<li><a href='index.php'>Blog</a></li>
+	<li><a href='categories.php'>Categories</a></li>
 	<li><a href='users.php'>Users</a></li>
 	<li><a href="../" target="_blank">View Website</a></li>
 	<li><a href='logout.php'>Logout</a></li>
