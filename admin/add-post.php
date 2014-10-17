cbc31aa950c01da184782eceeeec1fb95052109f
rocketpastsix <mjftrask@gmail.com>
2014-10-16 20:50:10 -0400

    some javascript. yay

--- admin/add-post.php
+++ admin/add-post.php
@@ -20,3 +20,16 @@ if(!$user->is_logged_in()){ header('Location: login.php'); }
 	<p><input type='submit' name='submit' value='Submit'></p>
 
 </form>
+
+<script src="//tinymce.cachefly.net/4.0/tinymce.min.js"></script>
+<script>
+        tinymce.init({
+            selector: "textarea",
+            plugins: [
+                "advlist autolink lists link image charmap print preview anchor",
+                "searchreplace visualblocks code fullscreen",
+                "insertdatetime media table contextmenu paste"
+            ],
+            toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
+        });
+</script>
