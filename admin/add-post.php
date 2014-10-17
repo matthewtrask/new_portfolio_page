e2a3881ff1b3a5164146819bd11814fd04bd9cac
rocketpastsix <mjftrask@gmail.com>
2014-10-16 20:46:43 -0400

    almost finished. thank god.

--- admin/add-post.php
+++ admin/add-post.php
@@ -5,3 +5,18 @@ require_once('../includes/config.php');
 //if not logged in redirect to login page
 if(!$user->is_logged_in()){ header('Location: login.php'); }
 ?>
+
+<form action='' method='post'>
+
+	<p><label>Title</label><br />
+	<input type='text' name='postTitle' value='<?php if(isset($error)){ echo $_POST['postTitle'];}?>'></p>
+
+	<p><label>Description</label><br />
+	<textarea name='postDesc' cols='60' rows='10'><?php if(isset($error)){ echo $_POST['postDesc'];}?></textarea></p>
+
+	<p><label>Content</label><br />
+	<textarea name='postCont' cols='60' rows='10'><?php if(isset($error)){ echo $_POST['postCont'];}?></textarea></p>
+
+	<p><input type='submit' name='submit' value='Submit'></p>
+
+</form>
